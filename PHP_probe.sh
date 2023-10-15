89a396b6b88f923d407c0f4c05955d3d8a1ee0b6
YuanCh <126393524+YuanLiuchang@users.noreply.github.com>
2023-10-15 15:43:54 +0800

    Update PHP_probe.sh
    Update PHP_probe.sh

--- PHP_probe.sh
+++ PHP_probe.sh
@@ -4,45 +4,41 @@
 echo "Welcome to the Automated PHP Probe Installation Script"
 echo
 
-# Prompt for PHP version
-read -p "Enter the PHP version (7.4/8.0, default is 7.4): " php_version
-php_version=${php_version:-7.4}
-
 # Prompt for the port
 read -p "Enter the desired port number (default is 8080): " port
 port=${port:-8080}
 
-# Check and install PHP
-if ! command -v php$php_version &> /dev/null; then
-    echo "PHP $php_version is not installed. Installing..."
-    sudo apt update
-    sudo apt install -y php$php_version
+# Check if PHP is installed
+if ! command -v php &> /dev/null; then
+    echo "PHP is not installed. Please install PHP before continuing."
+    exit 1
 fi
 
-# Check and install Git
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Installing..."
-    sudo apt update
-    sudo apt install -y git
+# Check if the Apache web server is installed
+if ! command -v apache2ctl &> /dev/null; then
+    echo "Apache web server is not installed. Please install Apache before continuing."
+    exit 1
 fi
 
-# Download and unzip the PHP probe script from GitHub
+# Download the PHP probe script
 echo "Downloading the PHP probe script..."
-wget -O PHP_probe.zip https://github.com/YuanLiuchang/PHP_probe/archive/refs/heads/main.zip
-unzip PHP_probe.zip -d /var/www/html
-mv /var/www/html/PHP_probe-main/PHP_probe.php /var/www/html/
-rm PHP_probe.zip
-rm -r /var/www/html/PHP_probe-main
+wget -O /var/www/html/PHP_probe.php https://raw.githubusercontent.com/YuanLiuchang/PHP_probe/main/PHP_probe.php
 
-# Enable a firewall (e.g., UFW)
-read -p "Do you want to enable the firewall? (y/n, default is n): " enable_firewall
-if [ "$enable_firewall" = "y" ]; then
-    sudo ufw allow $port/tcp
-    sudo ufw enable
-    echo "Firewall enabled, and port $port is allowed."
-else
-    echo "Firewall not enabled. Remember to configure your firewall manually if needed."
-fi
+# Create a virtual host configuration for Apache
+cat << EOF > /etc/apache2/sites-available/probe.conf
+<VirtualHost *:$port>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+# Enable the virtual host
+sudo a2ensite probe.conf
+
+# Reload Apache configuration
+sudo systemctl reload apache2
 
 # Display the access link
 echo
