7a6e14beb0851944e69273ebe17f01f6073e8d51
YuanCh <126393524+YuanLiuchang@users.noreply.github.com>
2023-10-15 15:59:12 +0800

    Update PHP_probe.sh

--- PHP_probe.sh
+++ PHP_probe.sh
@@ -4,6 +4,18 @@
 echo "Welcome to the Automated PHP Probe Installation Script"
 echo
 
+# Prompt for the web server choice
+echo "Choose a web server to install:"
+echo "1. Nginx (recommended, lower resource consumption)"
+echo "2. Apache (higher resource consumption)"
+read -p "Enter the number (1 or 2): " web_server_choice
+
+# Validate the choice
+if [ "$web_server_choice" != "1" ] && [ "$web_server_choice" != "2" ]; then
+    echo "Invalid choice. Please enter 1 for Nginx or 2 for Apache."
+    exit 1
+fi
+
 # Prompt for the port
 read -p "Enter the desired port number (default is 8080): " port
 port=${port:-8080}
@@ -14,18 +26,55 @@ if ! command -v php &> /dev/null; then
     exit 1
 fi
 
-# Check if the Apache web server is installed
-if ! command -v apache2ctl &> /dev/null; then
-    echo "Apache web server is not installed. Please install Apache before continuing."
-    exit 1
+# Install the selected web server (Nginx or Apache)
+if [ "$web_server_choice" = "1" ]; then
+    # Install Nginx
+    if ! command -v nginx &> /dev/null; then
+        echo "Nginx is not installed. Installing Nginx..."
+        sudo apt update
+        sudo apt install -y nginx
+    fi
+    web_server="Nginx"
+else
+    # Install Apache
+    if ! command -v apache2 &> /dev/null; then
+        echo "Apache is not installed. Installing Apache..."
+        sudo apt update
+        sudo apt install -y apache2
+    fi
+    web_server="Apache"
 fi
+
+# Create the PHP probe directory
+sudo mkdir -p /var/www/html
 
 # Download the PHP probe script
 echo "Downloading the PHP probe script..."
-wget -O /var/www/html/PHP_probe.php https://raw.githubusercontent.com/YuanLiuchang/PHP_probe/main/PHP_probe.php
+sudo wget -O /var/www/html/PHP_probe.php https://raw.githubusercontent.com/YuanLiuchang/PHP_probe/main/PHP_probe.php
 
-# Create a virtual host configuration for Apache
-cat << EOF > /etc/apache2/sites-available/probe.conf
+# Configure Nginx or Apache based on the choice
+if [ "$web_server_choice" = "1" ]; then
+    # Create an Nginx server block configuration
+    sudo tee /etc/nginx/sites-available/probe <<EOF
+server {
+    listen $port;
+    server_name localhost;
+
+    location / {
+        root /var/www/html;
+        index PHP_probe.php;
+    }
+
+    access_log /var/log/nginx/probe_access.log;
+    error_log /var/log/nginx/probe_error.log;
+}
+EOF
+
+    # Create a symbolic link to enable the Nginx server block
+    sudo ln -s /etc/nginx/sites-available/probe /etc/nginx/sites-enabled/
+else
+    # Create an Apache virtual host configuration
+    sudo tee /etc/apache2/sites-available/probe.conf <<EOF
 <VirtualHost *:$port>
     ServerAdmin webmaster@localhost
     DocumentRoot /var/www/html
@@ -34,13 +83,20 @@ cat << EOF > /etc/apache2/sites-available/probe.conf
 </VirtualHost>
 EOF
 
-# Enable the virtual host
-sudo a2ensite probe.conf
+    # Enable the Apache virtual host
+    sudo a2ensite probe.conf
+fi
 
-# Reload Apache configuration
-sudo systemctl reload apache2
+# Test the web server configuration and reload
+if [ "$web_server_choice" = "1" ]; then
+    sudo nginx -t
+    sudo systemctl reload nginx
+else
+    sudo apache2ctl configtest
+    sudo systemctl reload apache2
+fi
 
 # Display the access link
 echo
-echo "PHP probe has been installed successfully. You can access it at:"
-echo "http://your_server_ip:$port/PHP_probe.php"
+echo "PHP probe has been installed successfully with $web_server."
+echo "You can access it at: http://your_server_ip:$port/PHP_probe.php"
